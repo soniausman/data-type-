719422c9c91c2bd5eebfcf1aea8e30404686180b
soniausman <160847802+soniausman@users.noreply.github.com>
2024-03-04 21:59:16 +0500

    Add files via upload

---
+++ data_type.js
@@ -0,0 +1,14 @@
+//string
+var userName = "sonia usman";
+console.log(userName);
+//number
+var age = 30;
+console.log(age);
+//boolean
+var smoke = true;
+console.log(smoke);
+//null
+var ceime = null;
+//undefined
+var secret;
+console.log(secret);
---
+++ data_type.ts
@@ -0,0 +1,18 @@
+//string
+let userName ="sonia usman";
+console.log(userName);
+
+//number
+let age = 30;
+console.log(age);
+
+//boolean
+let smoke: boolean = true;
+console.log(smoke);
+
+//null
+let ceime= null;
+
+//undefined
+let secret:undefined;
+console.log(secret);
